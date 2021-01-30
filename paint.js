5c6fbb62e1eee61951b562f74aee225e7e0b60f1
kenatman <76776206+kenatman@users.noreply.github.com>
2021-01-30 13:42:34 +0900

    brush size

--- paint.js
+++ paint.js
@@ -4,6 +4,10 @@ const ctx = canvas.getContext("2d");
 
 const colors = document.getElementsByClassName("js-color");
 
+const range = document.getElementById("js-range");
+
+const mode = document.getElementById("jsMode");
+
 canvas.width = 700;
 canvas.height = 700;
 
@@ -11,6 +15,8 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+
+let filling = false;
 
 function startPainting() {
   painting = true;
@@ -37,6 +43,21 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handelRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -47,3 +68,11 @@ if (canvas) {
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handelRangeChange);
+}
+
+if (jsMode) {
+  mode.addEventListener("click", handleModeClick);
+}
